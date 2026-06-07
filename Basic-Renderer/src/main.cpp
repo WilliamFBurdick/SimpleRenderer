ab032300bcd2aad4166a91454f321acf8d89fc22
WilliamFBurdick <williamfburdick@gmail.com>
2026-06-07 17:40:03 -0400

    toon shading

--- Basic-Renderer/src/main.cpp
+++ Basic-Renderer/src/main.cpp
@@ -25,6 +25,39 @@ struct BlankShader : IShader {
 	}
 };
 
+struct ToonShader : IShader {
+	vec4 color;
+	const Model& model;
+	vec4 l;
+	vec4 varying_nrm[3];
+
+	ToonShader(const vec4 color, const vec3 light, const Model& m) : color(color), model(m) {
+		l = normalized((ModelView * vec4{ light.x, light.y, light.z, 0.0 }));
+	}
+
+	virtual vec4 vertex(const int face, const int vert) {
+		varying_nrm[vert] = ModelView.invert_transpose() * model.normal(face, vert);
+		vec4 gl_Position = ModelView * model.vert(face, vert);
+		return Perspective * gl_Position;
+	}
+
+	virtual std::pair<bool, TGAColor> fragment(const vec3 bar) const {
+		vec4 n = normalized(varying_nrm[0] * bar[0] + varying_nrm[1] * bar[1] + varying_nrm[2] * bar[2]);
+
+		double diffuse = std::max(0.0, n * l);
+
+		double intensity = 0.15 + diffuse;
+		if (intensity > 0.66) intensity = 1;
+		else if (intensity > 0.33) intensity = 0.66;
+		else intensity = 0.33;
+
+		TGAColor gl_FragColor;
+		for (int channel : {0, 1, 2})
+			gl_FragColor[channel] = std::min<int>(255, color[channel] * intensity);
+		return { false, gl_FragColor };
+	}
+};
+
 struct PhongShader : IShader {
 	const Model& model;
 	vec4 l;
@@ -93,12 +126,7 @@ void drop_zbuffer(std::string filename, std::vector<double>& zbuffer, int width,
 	zimg.write_tga_file(filename);
 }
 
-int main(int argc, char** argv) {
-	if (argc < 2) {
-		std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
-		return 1;
-	}
-
+void render_ssao_scene(int argc, char** argv) {
 	constexpr int width = 800;
 	constexpr int height = 800;
 	constexpr int shadoww = 8000;
@@ -136,8 +164,8 @@ int main(int argc, char** argv) {
 	auto smoothstep = [](double edge0, double edge1, double x) {
 		double t = std::clamp((x - edge0) / (edge1 - edge0), 0.0, 1.0);
 		return t * t * (3 - 2 * t);
-	};
-	
+		};
+
 #pragma omp parallel for
 	for (int x = 0; x < width; x++) {
 		for (int y = 0; y < height; y++) {
@@ -161,5 +189,68 @@ int main(int argc, char** argv) {
 	}
 
 	framebuffer.write_tga_file("framebuffer.tga");
+}
+
+void render_toon_scene(int argc, char** argv) {
+	constexpr int width = 800;
+	constexpr int height = 800;
+	constexpr vec3 light{ 1, 1, 1 };
+	constexpr vec3 eye{ -1, 0, 2 };
+	constexpr vec3 center{ 0, 0, 0 };
+	constexpr vec3 up{ 0, 1, 0 };
+
+	// standard rendering pass
+	lookat(eye, center, up);
+	init_perspective(norm(eye - center));
+	init_viewport(width / 16, height / 16, width * 7 / 8, height * 7 / 8);
+	init_zbuffer(width, height);
+	TGAImage framebuffer(width, height, TGAImage::RGB, { 177, 195, 209, 255 });
+
+	constexpr vec4 colors[] = { {22 * 4, 56 * 4, 147 * 4, 255}, {123, 98, 88, 255} };
+
+	for (int m = 1; m < argc; m++) {
+		Model model(argv[m]);
+		ToonShader shader(colors[(m - 1) % 2], light, model);
+		for (int f = 0; f < model.nfaces(); f++) {
+			Triangle clip = {
+				shader.vertex(f, 0),
+				shader.vertex(f, 1),
+				shader.vertex(f, 2)
+			};
+			rasterize(clip, shader, framebuffer);
+		}
+	}
+
+	// post-processing: edge detection
+	constexpr double threshold = 0.15;
+	for (int y = 1; y < framebuffer.height() - 1; ++y) {
+		for (int x = 1; x < framebuffer.width() - 1; ++x) {
+			vec2 sum;
+			for (int j = -1; j <= 1; ++j) {
+				for (int i = -1; i <= 1; ++i) {
+					constexpr int Gx[3][3] = { {-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1} };
+					constexpr int Gy[3][3] = { {-1, -2, -1}, {0, 0, 0}, {1, 2, 1} };
+					sum = sum + vec2{
+						Gx[j + 1][i + 1] * zbuffer[x + i + (y + j) * width],
+						Gy[j + 1][i + 1] * zbuffer[x + i + (y + j) * width]
+					};
+				}
+			}
+			if (norm(sum) > threshold)
+				framebuffer.set(x, y, TGAColor{ 0, 0, 0, 255 });
+		}
+	}
+	framebuffer.write_tga_file("toon-shading.tga");
+}
+
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		std::cerr << "Usage: " << argv[0] << " obj/model.obj" << std::endl;
+		return 1;
+	}
+
+	render_ssao_scene(argc, argv);
+	render_toon_scene(argc, argv);
+	
 	return 0;
 }
